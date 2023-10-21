bbb6b750cc49ec39b8f3dd4a8f7c2066629c22a8
Ocean <zouhaiyang2005@gmail.com>
2023-10-21 23:51:13 +1100

    change the function name db

--- server/src/loader/db.js
+++ server/src/loader/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports=async function main() {
+module.exports=async function db() {
   await mongoose.connect(process.env.DB);
   console.log("database connected")
 }
