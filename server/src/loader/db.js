c6394fbb88fbabc68064a27d3c8db7a0c2312241
Bruce <xden0006@gmail.com>
2023-10-28 10:34:14 +0800

    connect to mongoDB

--- server/src/loader/db.js
+++ server/src/loader/db.js
@@ -1,1 +1,22 @@
 //TODO: connect to mongoDB
+const { MongoClient } = require("mongodb");
+const username = encodeURIComponent("xden0006");
+const password = encodeURIComponent("yWnYSKgHtchhkhQ6");
+const cluster = "<clusterName>";
+const authSource = "<authSource>";
+const authMechanism = "<authMechanism>";
+let uri =
+  `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("<dbName>");
+    const ratings = database.collection("<collName>");
+    const cursor = ratings.find();
+    await cursor.forEach(doc => console.dir(doc));
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
